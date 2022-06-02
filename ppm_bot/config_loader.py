afaba5d2b52e603a2ad1ad54480c981268ee6799
Frantisek Stavela <fstavela@redhat.com>
2022-06-02 22:57:43 +0200

    Add config_loader handling of conf dir not found

--- ppm_bot/config_loader.py
+++ ppm_bot/config_loader.py
@@ -8,18 +8,23 @@ logger = logging.getLogger(__name__)
 
 
 def get_conf_path():
-    path = os.path.abspath(__file__)
-    split_path = path.split(os.sep)
+    file_path = os.path.abspath(__file__)
+    split_path = file_path.split(os.sep)
     while split_path[-1] != "ppm-bot":
         split_path.pop()
     split_path.append("conf")
-    return os.sep.join(split_path)
+    conf_path = os.sep.join(split_path)
+    if os.path.isdir(conf_path):
+        logger.debug(f"Found conf directory: {conf_path}")
+        return conf_path
+    raise NotADirectoryError(f"{conf_path} directory not found")
 
 
 def load_config(*conf_files: str):
     if not conf_files:
         default_file_names = ("config.default.yaml", "config.local.yaml")
-        conf_files = (os.path.join(get_conf_path(), file) for file in default_file_names)
+        conf_dir = get_conf_path()
+        conf_files = (os.path.join(conf_dir, file) for file in default_file_names)
     config = {}
     for file_name in conf_files:
         try:
